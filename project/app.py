8a427557fa7f1a9817eb75125cbb3767c2aacace
csrirammganesh <sriramearning23@gmail.com>
2025-12-18 11:32:33 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+# Read ENV variables
+port = int(os.environ.get("PORT", 5000))
+env = os.environ.get("FLASK_ENV", "development")
+
 @app.route("/")
 def hello():
-    return "Hello from Flask container"
+    return f"Hello! Running in {env} mode on port {port}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=port)
